2957be111a7001e98e55e6b4a2dfee770fd7bdbd
Peter Velas <peter.velas@lablabs.io>
2022-09-28 13:56:37 +0200

    Fix list of allowed arns.

--- iam.tf
+++ iam.tf
@@ -53,10 +53,8 @@ data "aws_iam_policy_document" "this" {
     actions = [
       "iam:PassRole"
     ]
-    resources = [
-      var.karpenter_node_role_arns[0]
-    ]
-    effect = "Allow"
+    resources = var.karpenter_node_role_arns
+    effect    = "Allow"
   }
 }
 
